89a148bab510861f4fef5c6fedc1a8ec5b3283fd
Duvan <duvanjarin@gmail.com>
2020-06-16 16:26:25 -0500

    fix 3-quick_sort.c

--- sort.h
+++ sort.h
@@ -23,5 +23,10 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int partition(int *array, size_t size, int low, int high);
+void partition_lomuto(int *array, size_t size, int low, int high);
+void quick_sort(int *array, size_t size);
 
 #endif
